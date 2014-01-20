a421ea461f9dd61adad56c9c12d41a00cc59cceb
ktjjang01 <ktjjang01@gmail.com>
2014-01-19 20:16:01 -0800

    fixed tab spaces

--- test/com/landon/FlushChecker/FlushTest.java
+++ test/com/landon/FlushChecker/FlushTest.java
@@ -6,32 +6,32 @@ public class FlushTest {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         boolean playAgain = false;
-        
+
         do {
-	        Introduction();
-	        int numCards = getNumCards(console);
-	
-	        Hand hand = new Hand(numCards);
-	        hand.deal();
-	
-	        printCards(hand);
-	
-	        if (hand.getNumCards() == 1) {
-	            System.out.println("You only have one suit: "
-	                    + hand.getHand()[0].getSuitAsString());
-	        } else { 
-		        if (isFlush(hand)) {
-		            System.out.println("You have a flush! Your cards have: "
-		                    + hand.getHand()[0].getSuitAsString());
-		        } else {
-		            System.out.println("You do not have a flush.");
-		        }
-	        }
-	        
-	        System.out.print("Would you like to play again? [Y/n] ");
-	        playAgain = console.next().equals("Y");
+            Introduction();
+            int numCards = getNumCards(console);
+
+            Hand hand = new Hand(numCards);
+            hand.deal();
+
+            printCards(hand);
+
+            if (hand.getNumCards() == 1) {
+                System.out.println("You only have one suit: "
+                        + hand.getHand()[0].getSuitAsString());
+            } else {
+                if (isFlush(hand)) {
+                    System.out.println("You have a flush! Your cards have: "
+                            + hand.getHand()[0].getSuitAsString());
+                } else {
+                    System.out.println("You do not have a flush.");
+                }
+            }
+
+            System.out.print("Would you like to play again? [Y/n] ");
+            playAgain = console.next().equals("Y");
         } while (playAgain);
-        
+
         console.close();
     }
 
